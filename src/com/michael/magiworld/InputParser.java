fe03fed1f46b621a32e8be122175d78a16e68622
Michael De Madet <michaeldemadet@gmail.com>
2019-05-09 03:14:24 +0200

    Add JavaDoc

--- src/com/michael/magiworld/InputParser.java
+++ src/com/michael/magiworld/InputParser.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class InputParser {
 
     /**
-     * Check inputValue,
-     * If inputValue is invalid, show error message until inputValue is valid,
-     * Then return inputValue.
-     * @param lowerLimit
-     * @param higherLimit
-     * @param errorMessage
-     * @return the correct inputValue
+     * Check the input value,
+     * If the input value is invalid, show error message until inputValue is valid,
+     * Then if the input value is correct, returns it.
+     * @param sc the input scanner
+     * @param lowerLimit lower limit value for the input value
+     * @param higherLimit higher limit value for the input value
+     * @param errorMessage message to show if the input value is invalid
+     * @return the correct input value
      */
     public int parseInput(Scanner sc, int lowerLimit, int higherLimit, String errorMessage) {
         int inputValue = 0;
@@ -37,6 +38,15 @@ public class InputParser {
         return inputValue;
     }
 
+    /**
+     * Check the input Value,
+     * If the input value is not equal to the condition, show error message until inputValue is valid,
+     * Then if the input value is equal to the condition, returns it.
+     * @param sc the input scanner
+     * @param condition the condition input value has to be equal
+     * @param errorMessage  message to show if the input value is not equal to the condition
+     * @return the correct input value
+     */
     public int parseInputForIntelligence(Scanner sc, int condition, String errorMessage) {
         int inputValue = 0;
         boolean goodResponse;
